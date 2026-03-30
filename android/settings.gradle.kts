0d080f1d77ec3acc0ceee5bd9faa555a6b7abad8
Wayd <wychancf@connect.ust.hk>
2026-03-30 19:03:07 +0800

    feat: integrate Firebase services with configuration files for Android, iOS, and macOS

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,6 +19,9 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.13.2" apply false // Do not upgrade to AGP 9 until flutter supports it
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services") version("4.3.15") apply false
+    // END: FlutterFire Configuration
     id("org.jetbrains.kotlin.android") version "2.3.10" apply false
 }
 
